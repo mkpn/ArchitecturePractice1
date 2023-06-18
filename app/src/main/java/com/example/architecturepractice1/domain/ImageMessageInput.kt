0a2703fa7a53fc4a08391f2923db771bc00512b5
neesan <exp.ym.lvl@gmail.com>
2023-06-19 01:08:11 +0900

    ImageMessageInputにリネーム

--- app/src/main/java/com/example/architecturepractice1/domain/ImageMessageInput.kt
+++ app/src/main/java/com/example/architecturepractice1/domain/ImageMessageInput.kt
@@ -2,16 +2,16 @@ package com.example.architecturepractice1.domain
 
 import android.graphics.Bitmap
 
-data class ImageMessageInputValidator(
-    val image: Bitmap?,
-    val text: String?
+data class ImageMessageInput(
+    var image: Bitmap?,
+    var text: String?
 ) {
     val isValid: Boolean
         get() {
             if (image == null) {
                 return false
             }
-            if (text != null && text.length > 80) {
+            if (text != null && text!!.length > 80) {
                 return false
             }
             return true
